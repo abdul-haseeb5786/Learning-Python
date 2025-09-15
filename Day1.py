b8d05fa41a8149b99449d0b221b713f26d8c0aa4
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-09-16 00:15:14 +0500

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -5,3 +5,9 @@ print(1232.5)
 print("Hello", "World")
 print("Hello", 123, True)
 print("Hello", "World", 123, sep="#")
+
+
+# End parameter in print
+print("Hello", end="\n")
+print("World!", end="\n")
+print(123)
