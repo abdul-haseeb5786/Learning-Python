d77fb70f6bfdfd2ee042beb2c5f3bbd7cdb14eec
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-09-26 07:47:04 -0700

    Update Day2.py

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -10,3 +10,12 @@ print(f"Using f-string: Dear your name is {name}, your age is {age} and you are 
 
 # Math inside f-string
 print(f"The price is {12*12} Dollars")
+
+# ================================
+# TYPE CONVERSION
+# ================================
+# Implicit
+print("\n--- Implicit Type Conversion ---")
+print(9 + 9.5)      # int + float
+print(5 + 6+7j)     # int + complex
+print(2.5 + 9 + 5j) # float + complex
