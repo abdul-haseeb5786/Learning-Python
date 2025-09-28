8f9812b3c546f8b34bfaf1d4cccfd6a10c64c693
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-09-28 03:39:31 -0700

    Update Day3.py

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -80,3 +80,38 @@ if x > 10:
 else:
     print("Less than 10")
 
+# -----------------------------
+# Marksheet Program
+# -----------------------------
+name = input("Enter your name: ")
+sub1 = float(input("Enter Chemistry marks: "))
+sub2 = float(input("Enter Physics marks: "))
+sub3 = float(input("Enter Math marks: "))
+sub4 = float(input("Enter English marks: "))
+sub5 = float(input("Enter Urdu marks: "))
+
+obtained_marks = sub1 + sub2 + sub3 + sub4 + sub5
+total_marks = 500
+percentage = (obtained_marks / total_marks) * 100
+
+if 90 <= percentage <= 100:
+    grade = "A+"
+elif 80 <= percentage < 90:
+    grade = "A"
+elif 70 <= percentage < 80:
+    grade = "B"
+elif 60 <= percentage < 70:
+    grade = "C"
+elif 50 <= percentage < 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"""
+          CITY PUBLIC SCHOOL
+
+     Your name is: {name}
+     Your Percentage is: {percentage:.2f}%
+     Your Grade is: {grade}
+""")
+
