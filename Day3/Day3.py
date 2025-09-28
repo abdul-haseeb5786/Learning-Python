194095829aeb0c23cc3f0d34b8a5c2e1cf5cdab8
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-09-28 03:40:07 -0700

    Update Day3.py

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -132,3 +132,14 @@ if name == user_name:
 else:
     print("Your name is invalid")
 
+# -----------------------------
+# Find Greatest Number
+# -----------------------------
+a, b, c = 10, 20, 30
+
+if a > b and a > c:
+    print(f"{a} is greatest")
+elif b > a and b > c:
+    print(f"{b} is greatest")
+else:
+    print(f"{c} is greatest")
