625eafaab2a47965e05a590cb1e806019f39f5c1
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-09-28 03:39:44 -0700

    Update Day3 copy.py

--- Day3/Day3 copy.py
+++ Day3/Day3 copy.py
@@ -7,23 +7,6 @@
 
 
 
-# -----------------------------
-# Login Program
-# -----------------------------
-user_name = "Abdul Rehman"
-user_password = "1234"  # better to store as string
-
-name = input("Enter your name: ")
-
-if name == user_name:
-    password = input("Enter your password: ")
-    if password == user_password:
-        print("LOGIN APPROVED")
-    else:
-        print("Your password is invalid")
-else:
-    print("Your name is invalid")
-
 
 # -----------------------------
 # Find Greatest Number
