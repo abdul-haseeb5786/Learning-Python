863beab713da643724f0317a32f5fe645da281d1
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-09-28 03:40:04 -0700

    Update Day3 copy.py

--- Day3/Day3 copy.py
+++ Day3/Day3 copy.py
@@ -8,14 +8,3 @@
 
 
 
-# -----------------------------
-# Find Greatest Number
-# -----------------------------
-a, b, c = 10, 20, 30
-
-if a > b and a > c:
-    print(f"{a} is greatest")
-elif b > a and b > c:
-    print(f"{b} is greatest")
-else:
-    print(f"{c} is greatest")
