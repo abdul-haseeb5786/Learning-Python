fdd1d3ff6e0f679822df162867c31473c5e96e0e
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-09-28 03:39:47 -0700

    Update Day3.py

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -115,3 +115,20 @@ print(f"""
      Your Grade is: {grade}
 """)
 
+# -----------------------------
+# Login Program
+# -----------------------------
+user_name = "Abdul Rehman"
+user_password = "1234"  # better to store as string
+
+name = input("Enter your name: ")
+
+if name == user_name:
+    password = input("Enter your password: ")
+    if password == user_password:
+        print("LOGIN APPROVED")
+    else:
+        print("Your password is invalid")
+else:
+    print("Your name is invalid")
+
