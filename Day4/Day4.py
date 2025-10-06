e8df4b4f173617987c013e3d351d82ee8bcaf5ab
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-10-06 14:00:10 +0500

    Update Day4.py

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,1 +1,16 @@
-print("Hello Day4")
+# Loops
+# Loop is the method of programming which is used for the work that you want to repeat again and again.
+
+# Two types of loops in Python:
+# 1. While Loop
+# 2. For Loop
+
+# ======================
+# While Loop Examples
+# ======================
+
+# Syntax
+'''
+while condition:
+    code
+'''
