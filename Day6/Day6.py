07fe9e9bc6b55ab2268b58810a08fcbe203e70b0
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-10-08 13:52:41 +0500

    Update Day6.py

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,1 +1,13 @@
+# Day 6: Python Functions
+# Created by: Abdul Rehman
+# Last Edited: 28-08-25
 
+# -----------------------------
+# Introduction to Functions
+# -----------------------------
+# A function is a reusable block of code that performs a specific task.
+
+# Benefits:
+# 1. Modularity – Code is broken into smaller, manageable pieces.
+# 2. Reusability – Write once, use many times.
+# 3. Readability – Code becomes more organized and easy to maintain.
