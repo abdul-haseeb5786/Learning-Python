760b883844ef426f7fc1b96aa319ff85f5fb4f5c
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-10-08 13:53:02 +0500

    Update Day6.py

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -11,3 +11,19 @@
 # 1. Modularity – Code is broken into smaller, manageable pieces.
 # 2. Reusability – Write once, use many times.
 # 3. Readability – Code becomes more organized and easy to maintain.
+
+# -----------------------------
+# Example Function
+# -----------------------------
+def is_even(num):
+    '''
+    Function gives odd or even.
+    Input : valid integer
+    Output : 'Odd' or 'Even'
+    Created by : Abdul Rehman
+    Last Edit : 28-8-25
+    '''
+    if num % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
