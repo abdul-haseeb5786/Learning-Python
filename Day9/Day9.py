03397dac328480f377f9ee1e48d5d646ee195c64
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-10-14 13:44:00 +0500

    Update Day9.py

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -1,1 +1,29 @@
+# 🐍 Day 9: Object-Oriented Programming in Python (OOP)
+
+# -------------------------------
+# 🌟 Introduction to OOP
+# -------------------------------
+# OOP helps in building scalable, reusable, and maintainable applications.
+
+# Advantages:
+# - Clear structure
+# - Reusability
+# - DRY (Don’t Repeat Yourself)
+# - Easier debugging
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
