06d0bc9e5647fe3718f00c4d3923c002c6950bef
Abdul Haseeb <139561169+abdul-haseeb5786@users.noreply.github.com>
2025-10-14 13:44:19 +0500

    Update Day9.py

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -11,7 +11,19 @@
 # - DRY (Don’t Repeat Yourself)
 # - Easier debugging
 
+# Core Concepts:
+# 1. Class
+# 2. Object
+# 3. Method
+# 4. Encapsulation
+# 5. Inheritance
+# 6. Polymorphism
+# 7. Abstraction
 
+
+# -------------------------------
+# 1️⃣ Class and Object
+# -------------------------------
 
 
 
